e97a58c79d72aa2d9ff47ef15d6348342724d42a
PurpxPanda <116929120+PurpxPanda@users.noreply.github.com>
2023-03-09 17:27:05 -0600

    Added array for character types and an empty array

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
-// Assignment code here
+//const array for each character type
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const uniqueCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "?", "/"];
 
+//empty array to store user choices
+let userChoices = [];
+let passwordLength = 0;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
